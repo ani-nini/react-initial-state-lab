01506cf55d13bdfb8c5d627a02bb81dde2c3e210
anissa guembre <guembreani@gmail.com>
2021-06-01 10:56:08 -0400

    completed

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+import React from 'react'
+
+export default class Bomb extends React.Component{
+    // we use the constructor to set the INITIAL STATE
+    constructor(props) { 
+        super()
+        this.state = { 
+          secondLeft: props.initialCount
+        }
+      } 
+      render (){
+          if (this.state.secondLeft ===0){
+              return <h1>Bomb!</h1>
+          }
+          else {
+              return <h1>{this.state.secondLeft} seconds left before I go boom!</h1>
+          }
+
+      }
+}
